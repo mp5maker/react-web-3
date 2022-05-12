c348351e4c6afc40ae7480d987fe00eda7e06a17
mp5maker <khan.photon@gmail.com>
2022-05-12 18:31:39 +0600

    :sparkles: feat: added etherium for the app

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -1,12 +1,10 @@
 const hre = require("hardhat");
 
 const main = async () => {
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
-
-  await greeter.deployed();
-
-  console.log("Greeter deployed to:", greeter.address);
+  const Transactions = await hre.ethers.getContractFactory("Transactions");
+  const transactions = await Transactions.deploy();
+  await transactions.deployed();
+  console.log("Transactions deployed to:", transactions.address);
 };
 
 const runMain = async () => {
